998f2c9aee6ea6b8c9296cb7a1d99ac9fb585db5
Drew Taylor <drewtaylorm@gmail.com>
2019-05-04 23:03:52 -0400

    deriv and deriv-at-point finished

--- README.md
+++ README.md
@@ -1,12 +1,42 @@
 # Symbolic Differentiation
 
-Computer algebra system in Racket that can be run in the REPL.  Takes equations in list form and returns derivative, more functionality will be introduced over time.
+Computer algebra system in Racket that can be run in the REPL.  Input equations in list form:
 
-### Example use:
+```
+'(+ (^ x 9) (cos (* 2 x)) 5)
+```
+is the list form of:
+```
+x^9 + cos(2x) + 5
+```
+
+### Currently supported features:
+
+##### deriv
+
+Takes a function f(x), returns the derivative.
 
 ```
 > (deriv '(* 3 (cos (^ x -6))))
 '(* 3 (* (* -6 1 (^ x -7)) (* -1 (sin (^ x -6)))))
 ```
 
-Simplification not complete yet.
+Note: Simplification of the output is not implemented yet.
+
+##### deriv-at-point
+
+Takes a function f(x) and a value of x, returns the value of the derivative of f(x) at x = input value
+
+```
+> (deriv-at-point '(+ (* 3 (^ x -5)) (sin x) (^ x 9) x 3) 2)
+2304.3494781634527
+```
+
+##### evaluate
+
+Takes a function f(x) and a value of x, returns the value of f(x) at x = input value
+
+```
+> (evaluate '(* 3 (^ x 2)) 5)
+75
+```
--- math.rkt
+++ math.rkt
@@ -23,9 +23,45 @@
                                     (list '^ (cadr equation) (- (caddr equation) 1)))]
      [(eq? (car equation) 'x) 1]
      [(number? (car equation)) 0]
-     [else (display "error\n")]
+     [else (display "error: \"")
+           (display (car equation))
+           (display "\" is not supported as an operator\n")]
      )))
 
 (define deriv-at-point
   (lambda (equation point)
-    ))
+    (evaluate (deriv equation) point)))
+
+(define evaluate
+  (lambda (equation value)
+    (let ([eq (replace-x equation value)])
+      (evaluate-rec eq value))))
+
+(define evaluate-rec
+  (lambda (equation value)
+    (cond
+     [(number? equation) equation]
+     [(not (list? equation)) (display "error\n")]
+     [(null? equation) 0]
+     [(eq? (car equation) '+) (cond
+                               [(null? (cddr equation)) (evaluate-rec (cadr equation) value)]
+                               [else (+ (evaluate-rec (cadr equation) value)
+                                        (evaluate-rec (cons '+ (cddr equation)) value))])]
+     [(eq? (car equation) '*) (cond
+                               [(null? (cddr equation)) (evaluate-rec (cadr equation) value)]
+                               [else (* (evaluate-rec (cadr equation) value)
+                                        (evaluate-rec (cons '* (cddr equation)) value))])]
+     [(eq? (car equation) 'sin) (sin (evaluate-rec (cadr equation) value))]
+     [(eq? (car equation) 'cos) (cos (evaluate-rec (cadr equation) value))]
+     [(eq? (car equation) 'tan) (tan (evaluate-rec (cadr equation) value))]
+     [(eq? (car equation) '^) (expt (evaluate-rec (cadr equation) value) (evaluate-rec (caddr equation) value))]
+     [else (display "error: \"")
+           (display (car equation))
+           (display "\" is not supported as an operator\n")])))
+
+(define replace-x
+  (lambda (equation value)
+    (map (lambda (i) (cond
+                      [(eq? i 'x) value]
+                      [(list? i) (replace-x i value)]
+                      [else i])) equation)))
